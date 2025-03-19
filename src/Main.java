53c3bbcb60a52e014a20c0a3a8b786bbf8d16718
OtavioAGO <otavioago@outlook.com>
2025-03-19 20:23:35 -0300

    feat: Adicionado menu principal

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        leArquivo();
+        while (true){
+            exibeMenu();
+            Scanner sc = new Scanner(System.in);
+            try {
+                int opc = Integer.parseInt(sc.nextLine());
+                if (opc <= 0){
+                    System.out.println("Opção invalida.");
+                    continue;
+                }
+                if (opc == 1){
+                    leArquivo();
+
+                }
+            } catch (Exception e){
+                System.out.println("Caractere invalido.");
+                continue;
+            }
+
+        }
     }
     public static void leArquivo(){
         Path path = Paths.get("formulario.txt");
@@ -17,4 +37,13 @@ public class Main {
             e.printStackTrace();
         }
     }
+    public static void exibeMenu(){
+        System.out.println("1.Cadastrar um novo pet");
+        System.out.println("2.Alterar os dados do pet cadastrado");
+        System.out.println("3.Deletar um pet cadastrado");
+        System.out.println("4.Listar todos os pets cadastrados");
+        System.out.println("5.Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6.Sair");
+    }
+    public
 }
